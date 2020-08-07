2ae290735590a55afd0c5132290a3d187977bec9
Caio Incau <caioincau@gmail.com>
2020-08-07 12:07:42 -0300

    Some style to our main dashboard button

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,54 @@ class BytebankApp extends StatelessWidget {
         child: ScopedModelDescendant<DarkModeModel>(
           builder: (context, child, model) => MaterialApp(
             theme: model.darkMode ? ThemeData.dark() : defaultTheme(),
-            home: Scaffold(
-              appBar: AppBar(
-                title: Text('Dashboard'),
-              ),
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset('images/bytebank_logo.png'),
-                  ),
-                  Container(
-                    height: 120,
-                    width: 100,
-                    color: Colors.green,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.people,
-                        ),
-                        Text('Contacts')
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
+            home: Dashboard(),
           ),
         ));
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            color: Theme.of(context).primaryColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(
+                  Icons.people,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Contacts',
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
